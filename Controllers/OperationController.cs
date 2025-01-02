7672a3523d86236792f6212379b65e6c0a2feb29
sergiocastro85 <sergiocastro86@gmail.com>
2025-01-02 12:09:01 -0500

    first commit

---
+++ Controllers/OperationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+    }
+}
